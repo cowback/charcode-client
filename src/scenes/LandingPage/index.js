01e41679d28018996c53b990d185b204e5674a90
Diogo Korok <korokdiogo@gmail.com>
2017-10-20 16:02:04 -0200

    set an action on every access related button

--- src/scenes/LandingPage/index.js
+++ src/scenes/LandingPage/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import Panel from 'components/Panel'
 import Button from 'components/Button'
+import Input from 'components/Input'
+import Modal from 'components/Modal'
 
 import phone from '../../assets/phone.png'
 import logo from '../../assets/logo.png'
 
 import './landing-page.css'
 
-const Header = () => (
+const Header = ({onHeaderButtonClick}) => (
   <Panel
     className="header"
     tag="header"
@@ -23,7 +25,7 @@ const Header = () => (
       <img src={logo} width="50" />
       <h4>climalerta</h4>
     </Panel>
-    <Button small ghost>
+    <Button small ghost onButtonClick={onHeaderButtonClick}>
       entrar
     </Button>
   </Panel>
@@ -57,10 +59,53 @@ const LandingPagePanel = ({ children }) => (
 )
 
 class LandingPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      registerSteps: 0
+    }
+  }
+
+  onHeaderButtonClick() {
+    this.setState({
+      registerSteps: 1
+    })
+  }
+
+  onAccessButtonClick() {
+    this.setState({
+      registerSteps: 2
+    })
+  }
+
+  onCepButtonClick() {
+    this.setState({
+      registerSteps: 0
+    })
+  }
+
   render() {
+    const accessModalChildren = (
+      <Panel column centered>
+        <h3>Queijo</h3>
+        <Input type="text" placeholder="Telefone" onChange={(event) => console.log(event.target.value)} required />
+        <Input type="password" placeholder="Senha" onChange={(event) => console.log(event.target.value)} required />
+        <Button small ghost onClick={this.onAccessButtonClick}>Entrar</Button>
+      </Panel>
+    )
+    const cepModalChildren = (
+      <Panel column centered>
+        <h3>Goiabada</h3>
+        <Input type="text" placeholder="CEP" onChange={(event) => console.log(event.target.value)} required />
+        <Button small ghost onClick={this.onCepButtonClick}>Cadastrar CEP</Button>
+      </Panel>
+    )
+
     return (
       <main className="landing-page" style={{ paddingTop: '4rem' }}>
-        <Header />
+        <Modal isOpen={this.state.registerSteps === 1} children={accessModalChildren} />
+        <Modal isOpen={this.state.registerSteps === 2} children={cepModalChildren} />
+        <Header onButtonClick={this.onHeaderButtonClick} />
         <LandingPagePanel>
           <h2>Lorem ipsum dolor sit amet.</h2>
           <div className="phone-image" />
