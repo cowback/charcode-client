b2d484a91b957a19084067d821c767a1708e9e3b
renanpvaz <renanp.vaz@outlook.com>
2017-10-20 14:03:59 -0200

    add basic landing page

---
+++ src/scenes/LandingPage/index.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+import Panel from 'components/Panel'
+import Button from 'components/Button'
+import phone from '../../assets/phone.png'
+
+import './landing-page.css'
+
+const Header = () => (
+  <Panel
+    className="header"
+    tag="header"
+    row
+    x="l"
+    align="center"
+    justify="space-between"
+    sizing="border"
+    fit
+  >
+    <h4>logo</h4>
+    <Button small ghost>
+      entrar
+    </Button>
+  </Panel>
+)
+
+const Footer = () => (
+  <Panel
+    tag="footer"
+    className="footer"
+    inset="l"
+    between="m"
+    row
+    justify="space-around"
+  >
+    <a target="_blank" href="https://github.com/cowback/">Contribua</a>
+    <a target="_blank" href="https://github.com/cowback/">Link</a>
+    <a target="_blank" href="https://github.com/cowback/">Outro link</a>
+  </Panel>
+)
+
+const LandingPagePanel = ({ children }) => (
+  <Panel
+    className="landing-page-panel"
+    column
+    x="l"
+    align="center"
+    justify="center"
+  >
+    {children}
+  </Panel>
+)
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <main className="landing-page" style={{ paddingTop: '4rem' }}>
+        <Header />
+        <LandingPagePanel>
+          <h2>Lorem ipsum dolor sit amet.</h2>
+          <div className="phone-image" />
+        </LandingPagePanel>
+        <LandingPagePanel>
+          <h2>Sed dorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+        </LandingPagePanel>
+        <LandingPagePanel>
+          <h2>Fique constantemente blabla alerta</h2>
+          <Button>
+            Começe agora
+          </Button>
+        </LandingPagePanel>
+        <Footer />
+      </main>
+    )
+  }
+}
+
+export default LandingPage
