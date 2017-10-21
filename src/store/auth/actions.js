6f705dda2ce9d09dc8d1233aa42c79bc728d5847
renanpvaz <renanp.vaz@outlook.com>
2017-10-21 04:37:30 -0200

    add logout ((again))

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -8,16 +8,15 @@ const login = credentials => (dispatch, _, api) => api.login(credentials)
     cookie.set('token', token)
     dispatch(setAuthState(true))
   })
-  .catch(console.error)
 
-const reauthenticate = () => (dispatch, _, api) => api.verifyToken()
-  .then(() => dispatch(setAuthState(true)))
-  .catch(console.error)
+const reauthenticate = () => (dispatch, _, api) => {
+  if (cookie.get().token)
+    api.verifyToken().then(() => dispatch(setAuthState(true)))
+}
 
 const createAccount = user => (dispatch, _, api) => api
   .createAccount(user)
   .then(() => dispatch(login(user)))
-  .catch(console.error)
 
 const logout = (phone) => dispatch => {
   dispatch(setAuthState(false))
